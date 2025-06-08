0599c4b2d935c662104f2851bf480dc8d091405e
ridwanstbd <ridwansetiobudi77@gmail.com>
2025-06-08 20:21:09 +0700

    fix: improve styling for TableCell component

--- src/components/Elements/TableRow/TableCell.jsx
+++ src/components/Elements/TableRow/TableCell.jsx
@@ -1,7 +1,13 @@
 const TableCell = ({ children, className }) => {
   return (
     <td
-      className={`px-6 py-4 whitespace-nowrap text-sm text-gray-500 ${className}`}
+      className={`
+        px-2 py-2 text-xs
+        sm:px-4 sm:py-3 sm:text-sm
+        md:px-6 md:py-4 md:text-base
+        whitespace-nowrap text-gray-500
+        ${className}
+      `}
     >
       {children}
     </td>
